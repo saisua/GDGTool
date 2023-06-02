9a37dc7d221409ab148ee3098dd2a4b00635b75c
Ausias <ausias.prieto@tyris.ai>
2023-06-02 09:30:13 +0200

    Entry scripts now accept arguments

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 xhost +local:docker
 docker-compose up --build -d && \
-docker exec -it scraper python3 /home/scraper/gui.py
+docker exec -it scraper python3 /home/scraper/gui.py $@
 xhost -local:docker
 docker stop scraper
--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #docker run -ti --rm --name scraper -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -d scraper
 echo "This will NOT build the docker"
 xhost +local:docker
-docker-compose up
+docker-compose up -d && \
+docker exec -it scraper python3 /home/scraper/gui.py $@
 xhost -local:docker
